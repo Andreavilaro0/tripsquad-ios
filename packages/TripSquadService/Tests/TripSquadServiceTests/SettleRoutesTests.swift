8ed2a51ec98a05b32783c396a1de12d002835d33
andrea.avila <andrea.avila@alumnos.udit.es>
2026-07-24 14:38:41 +0200

    fix(merge): helper deps(ahora:) de SettleRoutesTests incluye casosChat (merge M1->M6)

--- packages/TripSquadService/Tests/TripSquadServiceTests/SettleRoutesTests.swift
+++ packages/TripSquadService/Tests/TripSquadServiceTests/SettleRoutesTests.swift
@@ -280,6 +280,7 @@ struct SettleRoutesTests {
                 casosViaje: CasosDeUsoViaje(repo: repo),
                 casosVotacion: CasosDeUsoVotacion(repo: repo, membresia: repo, viajes: repo),
                 casosItinerario: CasosDeUsoItinerario(repo: repo, membresia: repo, viajes: repo),
+                casosChat: CasosDeUsoChat(repo: repo, membresia: repo),
                 repo: repo, pingBD: { true },
                 verificador: VerificadorSupabase(fuente: FuenteFalsa(jwks(Self.clave)), issuer: issDePrueba, audiencia: audDePrueba),
                 ahora: ahora)
